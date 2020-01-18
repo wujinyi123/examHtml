df1add398e90454628b6e99705b286d3e6d54156
wujinyi123 <996019864@qq.com>
2020-01-18 13:56:42 +0800

    000

--- html/student/views/console.js
+++ html/student/views/console.js
@@ -49,7 +49,7 @@ layui.use(['form','layer'], function(){
             url: "/back/exam/getExamByCode",
             dataType: "json",
             contentType: "application/json;charset=utf-8",
-            data: JSON.stringify({userType:"student", examCode:$('#examCode').val()}),
+            data: JSON.stringify({examCode:$('#examCode').val()}),
             success: function(data){
                 if (data.data.isFlag=='no') {
                     $("#examMsg").html(data.data.msg);
@@ -68,4 +68,60 @@ layui.use(['form','layer'], function(){
 
 function cleanExamMsg() {
     $("#examMsg").html("");
+}
+
+function getNewExam() {
+    layui.use(['laypage', 'layer'], function(){
+        var laypage = layui.laypage
+        ,layer = layui.layer;
+    
+        laypage.render({
+            elem: 'pageList'
+            ,count: 100
+            ,layout: ['count', 'prev', 'page', 'next', 'limit', 'refresh', 'skip']
+            ,jump: function(obj){
+              console.log(obj)
+            }
+        });
+    
+    });
+}
+
+function pageNewExam() {
+    layui.use('table', function () {
+        var table = layui.table;
+        table.render({
+            elem: '#pageNewExam',
+            url: '/back/exam/pageNewExam',
+            page: { //支持传入 laypage 组件的所有参数（某些参数除外，如：jump/elem） - 详见文档
+                layout: ['limit', 'count', 'prev', 'page', 'next', 'skip'], //自定义分页布局
+                limits: [5, 10, 15],
+                limit: 5,
+                curr: 1,
+                groups: 5, //只显示 1 个连续页码
+                first: '首页',
+                last: '尾页',
+                skin: 'row',
+                page: true,
+                even: true,
+                done: function (res) {
+                    userPage.data = res.data;
+                }
+            },
+            cols: [[
+                {field: 'examCode', title: '考试码', sort: true},
+                {field: 'examName', title: '考试名称', sort: true},
+                {field: 'score', title: '试卷总分', sort: true},
+                {field: 'teacherName', title: '出题教师', sort: true},
+                {field: 'time', title: '考试时间', sort: true},
+                {field: 'pdDate', title: '出题时间', sort: true},
+                {field: 'expDate', title: '截止时间', sort: true},
+                {
+                    field: 'id', title: '操作', sort: true, templet: function (data) {
+                        return '<a class="layui-btn layui-btn-mini links_edit" href="#" onclick="enterTest(\''+data.examCode+'\')"><i class="iconfont icon-edit"></i>进入考试</a>';
+                    }
+                }
+            ]]
+        });
+    });
 }
