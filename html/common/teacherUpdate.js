afc727ae199fcfd22f34343150f7353429322ed0
wujinyi123 <996019864@qq.com>
2020-04-09 17:33:56 +0800

    000

--- html/common/teacherUpdate.js
+++ html/common/teacherUpdate.js
@@ -1,5 +1,5 @@
-var stuNumber=window.location.href.split("=")[1];
-$('#stuNumber').html(stuNumber);
+var number=window.location.href.split("=")[1];
+$('#number').html(number);
 
 function bigger(imgSrc) {
     layui.use('layer', function () {
@@ -7,44 +7,16 @@ function bigger(imgSrc) {
     });
 }
 
-function changeClazz(flag,clazz) {
-    $('#clazz').html("");
-    var collegeCode = $("#collegeCode").val();
-    var year = $("#year").val();
+function makeSelect(sex,collegeCode) {
     $.ajax({
         type: "POST",
-        url: "/back/mb/listClazzByCY",
+        url: "/back/mb/listCollege",
         dataType: "json",
         contentType: "application/json;charset=utf-8",
-        data: JSON.stringify({collegeCode:collegeCode,year:year}),
         success: function(data){
             layui.use('form', function(){
                 $.each(data.data,function(index,value){
-                    $('#clazz').append('<option value="'+value.code+'">'+value.name+'</option>');
-                });
-                layui.form.render('select');
-            });
-            if (flag==1 || flag=='1') $('#clazz').val(clazz);
-        },
-        error:function(e){
-            console.log(e);
-        }
-    });
-}
-
-function makeSelect(sex,collegeCode,year,clazz) {
-    $.ajax({
-        type: "POST",
-        url: "/back/mb/collegeAndYear",
-        dataType: "json",
-        contentType: "application/json;charset=utf-8",
-        success: function(data){
-            layui.use('form', function(){
-                $.each(data.data.college,function(index,value){
                     $('#collegeCode').append('<option value="'+value.code+'">'+value.name+'</option>');
-                });
-                $.each(data.data.year,function(index,value){
-                    $('#year').append('<option value="'+value.code+'">'+value.name+'</option>');
                 });
                 $('#sex').append('<option value="M">男</option>');
                 $('#sex').append('<option value="G">女</option>');
@@ -52,8 +24,6 @@ function makeSelect(sex,collegeCode,year,clazz) {
             });
             $('#sex').val(sex);
             $('#collegeCode').val(collegeCode);
-            $('#year').val(year);
-            changeClazz(1,clazz);
         },
         error:function(e){
             console.log(e);
@@ -66,13 +36,13 @@ $.ajax({
     url: "/back/user/getUserInfo",
     dataType: "json",
     contentType: "application/json;charset=utf-8",
-    data: JSON.stringify({userType:"student",number:stuNumber}),
+    data: JSON.stringify({userType:"teacher",number:number}),
     success: function(data){
         $('#img').append('<img id="img" src="'+data.data.img+'" onclick="bigger(\''+data.data.img+'\')" style="width: 100px;">')
-        $('#stuName').val(data.data.name);
+        $('#name').val(data.data.name);
         $('#tel').val(data.data.tel);
         $('#email').val(data.data.email);
-        makeSelect(data.data.sexCode,data.data.collegeCode,data.data.year,data.data.clazz);
+        makeSelect(data.data.sexCode,data.data.collegeCode);
     },
     error:function(e){
         console.log(e);
@@ -80,23 +50,15 @@ $.ajax({
 });
 
 layui.use(['form','layer'], function () {
-    layui.form.on("select(collegeCode)",function () {
-        changeClazz(0,"");
-    });
-
-    layui.form.on("select(year)",function () {
-        changeClazz(0,"");
-    });
-
     layui.form.on('submit(changeInfo)', function(dataForm){
         jsonData = {
-            userType:"student",
-            number:$('#stuNumber').html(),
-            name:$('#stuName').val(),
+            userType:"teacher",
+            number:$('#number').html(),
+            name:$('#name').val(),
             sex:$('#sex').val(),
             tel:$('#tel').val(),
             email:$('#email').val(),
-            clazz:$('#clazz').val()
+            collegeCode:$('#collegeCode').val()
         };
         $.ajax({
             type: "POST",
@@ -116,8 +78,8 @@ layui.use(['form','layer'], function () {
 
     layui.form.on('submit(changePassword)', function(dataForm){
         jsonData = {
-            userType:"student",
-            number:$('#stuNumber').html()
+            userType:"teacher",
+            number:$('#number').html()
         };
         $.ajax({
             type: "POST",
